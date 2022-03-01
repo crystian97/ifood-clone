bd8c11018fb88e83cff18a48b27e1b8da9bec480
crystian97 <icrystianveloso@gmail.com>
2022-03-01 11:13:46 -0300

    Receiving data from API

--- src/pages/Principal/index.js
+++ src/pages/Principal/index.js
@@ -1,11 +1,44 @@
-
+import React ,{useState,useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
+import { Text ,Alert,ActivityIndicator} from 'react-native';
 import { SafeAreaView,Image,View,ButtonContainer } from './style';
 
-export default function Home() {
+export default function Principal() {
+  
+  // hooks
+  // statess
+  const [banners,setBanner] = useState([])
+  const [categorias,setCategorias] = useState([])
+  const [restaurantes,setRestautantes] = useState([])
+  // load
+  const [loaded,setLoaded] = useState(false)
+
+  // effects
+  useEffect(()=>{
+async function buscaDados(){
+try{
+
+  const response = await fetch('http://my-json-server.typicode.com/pablohdev/app-ifood-clone/db');
+  console.log(response)
+
+  const data = await response.json();
+  console.log(data);
+  setLoaded(true);
+  setBanner(data.banner_principal);
+  setCategorias(data.categorias)
+  setRestautantes(data.restaurantes)
+  Alert.alert(data)
+}catch(e){
+Alert.alert('Erro ao consultar' + e)
+}
+}
+buscaDados();
+  },[])
+
   return (
+    
     <>
+    <StatusBar style = "theme-dark"/>
     <SafeAreaView>
   
       <Text>Principal</Text>
